666321043ccfe632bf341633d319e4510e4b7a3d
EgorFray <fatamorganaa933@gmail.com>
2025-10-30 20:22:03 +0200

    Create some reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Row from "./ui/Row";
 
 function App() {
 	return (
 		<>
 			<GlobalStyles />
+			<Row type="horizontal">
+				<Button variation="primary" size="medium">
+					Test
+				</Button>
+				<Button variation="secondary" size="small">
+					test2
+				</Button>
+			</Row>
+			<Row></Row>
 			<div>Hello world</div>
 		</>
 	);
