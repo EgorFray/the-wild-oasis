f22e33b89cb30f1a73ae717f822918f6baa84f26
EgorFray <fatamorganaa933@gmail.com>
2025-11-16 19:00:07 +0200

    Editing a cabin

--- src/features/cabins/CreateCabinForm-v1.jsx
+++ src/features/cabins/CreateCabinForm-v1.jsx
@@ -8,7 +8,7 @@ import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import FormRow from "../../ui/FormRow";
 
-import { createEditCabin } from "../../services/apiCabins";
+import { createCabin } from "../../services/apiCabins";
 import { toast } from "react-hot-toast";
 
 function CreateCabinForm({ cabinToEdit = {} }) {
@@ -20,9 +20,8 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 	});
 	const { errors } = formState;
 	const queryClient = useQueryClient();
-
-	const { mutate: createCabin, isLoading: isCreating } = useMutation({
-		mutationFn: createEditCabin,
+	const { mutate, isLoading: isCreating } = useMutation({
+		mutationFn: createCabin,
 		onSuccess: () => {
 			toast.success("New cabin successfully created");
 			queryClient.invalidateQueries({ queryKey: ["cabins"] });
@@ -31,22 +30,8 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 		onError: (err) => toast.error(err.message),
 	});
 
-	const { mutate: editCabin, isLoading: isEditing } = useMutation({
-		mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
-		onSuccess: () => {
-			toast.success("Cabin successfully edited");
-			queryClient.invalidateQueries({ queryKey: ["cabins"] });
-			reset();
-		},
-		onError: (err) => toast.error(err.message),
-	});
-
-	const isWorking = isCreating || isEditing;
-
 	function onSubmit(data) {
-		const image = typeof data.image === "string" ? data.image : data.image[0];
-		if (isEditSession) editCabin({ newCabinData: { ...data, image }, id: editId });
-		else createCabin({ ...data, image: image });
+		mutate({ ...data, image: data.image.at(0) });
 	}
 
 	function onError(errors) {}
@@ -57,7 +42,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Input
 					type="text"
 					id="name"
-					disabled={isWorking}
+					disabled={isCreating}
 					{...register("name", { required: "This field is required" })}
 				/>
 				{errors?.name?.message && <Error>{errors.name.message}</Error>}
@@ -67,7 +52,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Input
 					type="number"
 					id="maxCapacity"
-					disabled={isWorking}
+					disabled={isCreating}
 					{...register("maxCapacity", {
 						required: "This field is required",
 						min: { value: 1, message: "Capacity should be at least 1" },
@@ -79,7 +64,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Input
 					type="number"
 					id="regularPrice"
-					disabled={isWorking}
+					disabled={isCreating}
 					{...register("regularPrice", {
 						required: "This field is required",
 						min: { value: 1, message: "Capacity should be at least 1" },
@@ -92,7 +77,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 					type="number"
 					id="discount"
 					defaultValue={0}
-					disabled={isWorking}
+					disabled={isCreating}
 					{...register("discount", {
 						required: "This field is required",
 						validate: (value) =>
@@ -106,7 +91,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Textarea
 					type="number"
 					id="description"
-					disabled={isWorking}
+					disabled={isCreating}
 					defaultValue=""
 					{...register("description", { required: "This field is required" })}
 				/>
@@ -127,7 +112,9 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 				<Button variation="secondary" type="reset">
 					Cancel
 				</Button>
-				<Button disabled={isWorking}>Edit cabin</Button>
+				<Button disabled={isCreating}>
+					{isEditSession ? "Edit cabin" : "Create new cabin"}
+				</Button>
 			</FormRow>
 		</Form>
 	);
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,12 +10,23 @@ export async function getCabins() {
 }
 
 // https://buuxjvhzyqcjqyupkere.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin, id) {
+	const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
 	const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "");
-	const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
-	const { data, error } = await supabase
-		.from("cabins")
-		.insert([{ ...newCabin, image: imagePath }]);
+	const imagePath = hasImagePath
+		? newCabin.image
+		: `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+	let query = supabase.from("cabins");
+
+	// FOR CREATE
+	if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+	const { data, error } = await query.select().single();
+
+	// FOR EDIT
+	if (id) query = query.update({ ...newCabin, image: imagePath }).eq("id", id);
+
 	if (error) {
 		console.error(error);
 		throw new Error("Cabin could not be created");
