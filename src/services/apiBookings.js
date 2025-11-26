50e5869ff328fdb56e7616824135f01eecf978ac
EgorFray <fatamorganaa933@gmail.com>
2025-11-26 18:59:30 +0200

    Prefectching with react query

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -17,6 +17,7 @@ export async function getBookings({ filter, sortBy, page }) {
 	if (sortBy)
 		query = query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
 
+	// PAGINATION
 	const from = (page - 1) * PAGE_SIZE;
 	const to = form + PAGE_SIZE - 1;
 	if (page) query = query.range(from, to);
