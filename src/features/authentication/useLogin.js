7e98e4a36f39bd8e224d3a860b78e47cc2338ddd
EgorFray <fatamorganaa933@gmail.com>
2025-12-18 14:44:51 +0200

    Authorization: protecting route

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+	const queryClient = useQueryClient();
 	const navigate = useNavigate();
 	const { mutate: login, isLoading } = useMutation({
 		mutationFn: ({ email, password }) => loginApi({ email, password }),
 		// In onSuccess we can receive data that was passed to the function as an input
 		onSuccess: (user) => {
-			console.log(user);
+			queryClient.setQueriesData(["user"], user);
 			navigate("/dashboard");
 		},
 		onError: (err) => {
