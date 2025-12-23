806f763a16fd410cd2e91bd50567035614b6c717
EgorFray <fatamorganaa933@gmail.com>
2025-12-23 21:55:25 +0200

    User signUp

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,21 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignup } from "./useSignup";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-	const { register, formState, getValues, handleSubmit } = useForm();
+	const { signup, isLoading } = useSignup();
+	const { register, formState, getValues, handleSubmit, reset } = useForm();
 	const { errors } = formState;
 
+	function onSubmit({ fullName, email, password }) {
+		signup({ fullName, email, password }, { onSettled: reset });
+	}
+
 	return (
-		<Form onSubmit={handleSubmit()}>
+		<Form onSubmit={handleSubmit(onSubmit)}>
 			<FormRow label="Full name" error={errors?.fullName?.message}>
 				<Input
 					type="text"
